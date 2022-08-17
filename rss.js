d235a0d3310124c5fce1dfd8f966f29e72b1f792
Ste Allen <steallen@gmail.com>
2022-08-17 19:01:11 +0100

    support for posting more than one item per feed update

--- rss.js
+++ rss.js
@@ -1,31 +1,58 @@
-const Source = require('./models/sources');
-const Parser = require('rss-parser');
-const parser = new Parser();
+const Source = require('./models/sources')
+const Parser = require('rss-parser')
+const parser = new Parser()
 
 class Feed {
   constructor(id, title, url, currentGuid, image) {
-    this.id = id;
-    this.title = title;
-    this.url = url;
-    this.currentGuid = currentGuid;
-    this.image = image;
+    this.id = id
+    this.title = title
+    this.url = url
+    this.currentGuid = currentGuid
+    this.image = image
   }
 
   async update() {
-    const parsed = await parser.parseURL(this.url);
-    const latest = parsed.items[0];
-    const guid = latest.guid || latest.id;
+    const parsed = await parser.parseURL(this.url)
+    const latest = parsed.items[0]
+    const guid = latest.guid || latest.id
     if (!this.currentGuid || this.currentGuid !== guid) {
-      this.currentGuid = guid;
       await Source.findByIdAndUpdate(this.id, {
-        currentGuid: this.currentGuid,
-      });
-      this.currentEp = latest;
-      return true;
+        currentGuid: guid,
+      })
+      this.currentEp = parsed.items
+        .slice(
+          0,
+          parsed.items.findIndex(
+            (item) =>
+              item.guid === this.currentGuid || item.id === this.currentGuid
+          ) || 1
+        )
+        .slice(0, 3)
+      this.currentGuid = guid
+      return true
     } else {
-      return;
+      return
     }
   }
 }
 
-module.exports = { Feed };
+async function inputSingleFeed(url) {
+  const feed = await parser.parseURL(url)
+  const image = feed.image ? feed.image.url : ''
+  if (feed.title) {
+    if (await Source.findOne({ url: url })) {
+      return false
+    }
+    await new Source({
+      url: url,
+      title: feed.title,
+      image: image,
+    }).save()
+    const dbFeed = await Source.findOne({ url: url })
+    return new Feed(dbFeed.id, dbFeed.title, dbFeed.url)
+  } else {
+    return false
+  }
+}
+
+module.exports = { Feed, inputSingleFeed }
