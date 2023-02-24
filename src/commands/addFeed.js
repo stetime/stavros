4858eb5a4faf656f0acfd2d10830228543b63407
Stephen Allen <steallen@gmail.com>
2023-02-24 16:02:53 +0000

    addfeed error handling + more content types supported

--- src/commands/addFeed.js
+++ src/commands/addFeed.js
@@ -13,22 +13,36 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    const input = interaction.options.getString('input')
-    const dupe = rss.sourceList.find((feed) => feed.url === input)
-    if (dupe) {
-      logger.debug(`attempt to add dupe feed: ${input}`)
-      await interaction.reply({
-        content: `already subscribed to ${dupe.title}`,
-        ephemeral: true,
-      })
-    } else {
-      const newFeed = await rss.inputSingleFeed(input)
-      rss.sourceList.push(newFeed)
-      logger.info(`${newFeed.title} - has been added to the db and sourcelist`)
-      logger.debug(rss.sourceList)
-      await interaction.reply({
-        content: `Subscribed to feed ${newFeed.title}`,
-      })
+    try {
+      const input = interaction.options.getString('input')
+      const dupe = rss.sourceList.find((feed) => feed.url === input)
+      if (dupe) {
+        logger.debug(`attempt to add dupe feed: ${input}`)
+        await interaction.reply({
+          content: `already subscribed to ${dupe.title}`,
+          ephemeral: true,
+        })
+      } else {
+        const newFeed = await rss.inputSingleFeed(input)
+        rss.sourceList.push(newFeed)
+        logger.info(`${newFeed.title} - has been added to the db and sourcelist`)
+        logger.debug(rss.sourceList)
+        await interaction.reply({
+          content: `Subscribed to feed ${newFeed.title}`,
+        })
+      }
+    } catch (error) {
+      if (error.message.includes('ECONNREFUSED')
+        || error.message.includes('40') 
+        || error.message.includes('Unexpected close')) {
+        await interaction.reply('That is not a valid feed.')
+      } else if (error.message.includes('429')) {
+        await interaction.reply('We are currently rate-limited. Try again later.')
+      } else {
+        await interaction.reply('Can\'t do that pal')
+        logger.error(error)
+      }
     }
-  },
+
+  }
 }
