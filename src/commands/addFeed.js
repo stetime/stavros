1f5f2663c540cb5768731d121b1dbb377367f55a
Stephen Allen <720493+stetime@users.noreply.github.com>
2025-01-08 15:35:07 +0000

    add interaction username to logs

--- src/commands/addFeed.js
+++ src/commands/addFeed.js
@@ -32,7 +32,7 @@ export const command = {
     }
     const dupe = sourceList.find((feed) => feed.url === input)
     if (dupe) {
-      logger.debug(`attempt to add dupe feed: ${input}`)
+      logger.debug(`attempt to add dupe feed: ${input} by ${interaction.user.username}`)
       await interaction.editReply({
         content: `already subscribed to ${dupe.title}`,
         ephemeral: true,
@@ -40,7 +40,7 @@ export const command = {
     } else {
       const newFeed = await inputSingleFeed(input)
       sourceList.push(newFeed)
-      logger.info(`${newFeed.title} - has been added to the db and sourcelist`)
+      logger.info(`${newFeed.title} - has been added to the db and sourcelist by ${interaction.user.username}`)
       logger.debug(JSON.stringify(sourceList, null, 2))
       await interaction.editReply({
         content: `Subscribed to feed ${newFeed.title}`,
