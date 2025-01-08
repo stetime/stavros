1954069300c67e90236bdf8f0e5dd7f0d34ebb42
Stephen Allen <720493+stetime@users.noreply.github.com>
2025-01-08 15:41:58 +0000

    update dbdump and gitignore

--- src/utils/dbDump.js
+++ src/utils/dbDump.js
@@ -3,12 +3,36 @@ import fs from 'fs/promises'
 
 
 async function dumpFeeds() {
-  await mongo.init()
   const feeds = await mongo.getFeeds()
   const jsonString = JSON.stringify(feeds, null, 2)
   await fs.writeFile('feeds.json', jsonString, 'utf-8')
 }
 
+async function dumpGames() {
+  const games = await mongo.getGames()
+  const jsonString = JSON.stringify(games, null, 2)
+  await fs.writeFile('games.json', jsonString, 'utf-8')
+}
+
+async function dumpNames() {
+  const names = await mongo.getNames()
+  const jsonString = JSON.stringify(names, null, 2)
+  await fs.writeFile('names.json', jsonString, 'utf-8')
+}
+
+async function dumpPrefixes() {
+  const prefixes = await mongo.getPrefixes()
+  const jsonString = JSON.stringify(prefixes, null, 2)
+  await fs.writeFile('prefixes.json', jsonString, 'utf-8')
+}
+await mongo.init()
+console.log('dumping feeds')
 await dumpFeeds()
+console.log('dumping games')
+await dumpGames()
+console.log('dumping names')
+await dumpNames()
+console.log('dumping prefixes')
+await dumpPrefixes()
 await mongo.close()
 process.exit(1)
